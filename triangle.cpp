80544c0e788fdf1f647e7a03669cb628ce7abf12
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2021-03-06 21:38:14 +0530

    Create triangle.cpp

---
+++ triangle.cpp
@@ -0,0 +1,12 @@
+// Program for Area of Triangle
+#include <iostream>
+using namespace std;
+
+int main() {
+	int area,length,breadth;
+	cout<<"Enter the length and Breadth of Triangle: \n";
+	cin>>length>>breadth;
+	area=(length*breadth)/2;
+	cout<<"The Area of Triangle is: "<<area;
+	return 0;
+}
