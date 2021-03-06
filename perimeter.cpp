0ed0ae45e622d545a94cafb704307512c1bac130
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2021-03-06 21:51:42 +0530

    Create perimeter.cpp

---
+++ perimeter.cpp
@@ -0,0 +1,13 @@
+// WAP for Perimeter of Rectangle
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int Perimeter,l,b;
+    cout<<"Enter the length and breadth of Rectangle: \n";
+    cin>>l>>b;
+    Perimeter = 2*(l+b);
+    cout<<"The Perimeter of Rectangle is: "<<Perimeter;
+    return 0;
+}
