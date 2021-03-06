171dd1da9205451f134569d4f000e3a11af5b95c
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2021-03-06 22:00:59 +0530

    Create natural.cpp

---
+++ natural.cpp
@@ -0,0 +1,14 @@
+// Finding sum of first n Natural numbers.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int sum,n;
+    cout<<"Enter the N Value: \n";
+    cin>>n;
+    sum=(n*(n+1))/2;
+    cout<<"Sum of N Term is: "<<sum;
+    return 0;
+}
